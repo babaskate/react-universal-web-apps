7507031d43be5cffd3188ee1fc61bbce54c1e5b3
babaskate <babaspamian@gmail.com>
2016-10-01 20:55:52 -0400

    add footer

--- app/components/app.js
+++ app/components/app.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import Header from './main/Header';
+import Footer from './main/Footer';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                <Header root={this.props.route.path}/>
+                <Header root={this.props.route.path} />
                 {this.props.children}
+                <Footer />
             </div>
         );
     }
---
+++ app/components/main/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="app-footer">
+                Common Footer
+            </footer>
+        );
+    }
+}
